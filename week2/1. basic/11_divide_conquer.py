9e6893be63df7f1c4b5e98cd7483a11b9c2449c2
jye-1123 <jyepjh1123@gmail.com>
2026-08-29 22:03:44 +0900

    분할 정복 풀이

--- week2/1. basic/11_divide_conquer.py
+++ week2/1. basic/11_divide_conquer.py
@@ -37,18 +37,23 @@ def find_max_divide_conquer(arr, left, right):
     """
     # TODO: base case - 원소가 하나면 그 값 반환
     pass
+    if left == right:
+        return arr[left]
     
     # TODO: 중간 지점 계산
     pass
+    mid = (left+right)//2
     
     # TODO: 왼쪽 절반의 최댓값
     pass
-        
+    left_divide_max = find_max_divide_conquer(arr, left, mid)
+    
     # TODO: 오른쪽 절반의 최댓값
     pass
-    
+    right_divide_max = find_max_divide_conquer(arr, mid+1, right)
+
     # TODO: 둘 중 큰 값 반환
-    pass
+    return left_divide_max if left_divide_max > right_divide_max else right_divide_max
 
 # 테스트 케이스
 if __name__ == "__main__":
