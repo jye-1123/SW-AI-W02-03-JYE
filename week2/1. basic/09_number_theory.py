73787fd1c3255374c53714ad759d08e5998cd999
jye-1123 <jyepjh1123@gmail.com>
2026-08-29 20:34:35 +0900

    정수론, 이분탐색 풀이

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -39,6 +39,12 @@ def gcd(a, b):
     # recursive를 이용 
     pass
 
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+
 def gcd_iterative(a, b):
     """
     반복문을 사용한 최대공약수 계산
@@ -51,7 +57,11 @@ def gcd_iterative(a, b):
     """
     # TODO: 반복문으로 구현
     # b가 0이 될 때까지 반복
-    pass
+    
+    while(b != 0) :
+        a, b = b, a%b
+    return a
+
 
 def lcm(a, b):
     """
@@ -65,6 +75,7 @@ def lcm(a, b):
     """
     # TODO: LCM 계산
     pass
+    return (a * b) // gcd(a, b)
 
 def extended_gcd(a, b):
     """
@@ -83,6 +94,14 @@ def extended_gcd(a, b):
     # 역추적하며 x, y 계산
     pass
 
+    if b == 0:
+        return a, 1, 0
+    else:
+        gcd, x1, y1 = extended_gcd(b, a%b)
+        x = y1
+        y = x1 - (a//b)*y1
+        return gcd, x, y
+
 def is_prime(n):
     """
     소수 판별
@@ -98,7 +117,23 @@ def is_prime(n):
     # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
     # 3부터 sqrt(n)까지 홀수만 확인
     pass 
-
+    import math
+    
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n%2 == 0:
+        return False
+    
+    for i in range(3, int(math.sqrt(n)), 2):
+        if i%n == 0:
+            return False
+        
+    return True
+        
+        
+        
 # 테스트 케이스
 if __name__ == "__main__":
     # 테스트 케이스 1: GCD와 LCM
