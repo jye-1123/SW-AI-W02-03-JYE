c3543625f0bba767082c56ecc843c5906d8e3b0b
jye-1123 <jyepjh1123@gmail.com>
2026-09-03 16:30:38 +0900

    1번 이진트리 문제 풀이

--- week3/1. basic/01_binary_tree.py
+++ week3/1. basic/01_binary_tree.py
@@ -39,21 +39,22 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 def preorder(root):
     """전위 순회: 루트 → 왼쪽 → 오른쪽"""
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
-    pass
+    if root is None:
+        return []
     
     # TODO: 루트 값 추가
-    pass
+    result.append(root.value)
     
     # TODO: 왼쪽 서브트리 순회
-    pass
-    
+    result += preorder(root.left)
     # TODO: 오른쪽 서브트리 순회
-    pass
+    result += preorder(root.right)
     
     return result
 
@@ -62,16 +63,17 @@ def inorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
-    pass
+    if root is None:
+        return []
     
     # TODO: 왼쪽 서브트리 순회
-    pass
+    result += inorder(root.left)
     
     # TODO: 루트 값 추가
-    pass
+    result.append(root.value)
     
     # TODO: 오른쪽 서브트리 순회
-    pass
+    result += inorder(root.right)
     
     return result
 
@@ -80,16 +82,17 @@ def postorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
-    pass
+    if root is None:
+        return []
     
     # TODO: 왼쪽 서브트리 순회
-    pass
+    result += postorder(root.left)
     
     # TODO: 오른쪽 서브트리 순회
-    pass
+    result += postorder(root.right)
     
     # TODO: 루트 값 추가
-    pass
+    result.append(root.value)
     
     return result
 
