341e0b2636bf44c33a229eaef35cf9e4e31eedb5
jye-1123 <jyepjh1123@gmail.com>
2026-09-05 13:39:19 +0900

    계단 문제 풀이

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -46,7 +46,15 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
+    while change == 0:
+        for i in coins:
+            cnt = change//i
+            change = change%i
+            
+            result.append(cnt)
+            total_coins += cnt
+            
+
     
     return total_coins, result
 
